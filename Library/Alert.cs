65797e8dc52c23b3e835ebd5ce916dc5254ac1c1
Santiago García <scgc612@gmail.com>
2020-09-21 22:47:35 -0300

    Critica segun Polimorfismo

--- Library/Alert.cs
+++ Library/Alert.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace LSPLibrary
 {
+    /// <summary>
+    /// El metodo Send no cumple con el patrón poliformismo
+    /// ya que el codigo que varía segun tiene distintos destinatarios
+    /// deberia estar en diferentes clases con una operación polimórfica
+    /// </summary>
     public class Alert
     {
         public void Send(string type, string text)
